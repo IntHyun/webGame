9515ea94290e8a04080f247e0a8921749e92b1aa
JeongSuHyun <68059880+baejeong12@users.noreply.github.com>
2022-11-07 21:46:32 +0900

    끝말잇기 js추가

--- WebGame/js/index.js
+++ WebGame/js/index.js
@@ -1,6 +1,12 @@
 const $startBtn = document.querySelector('.btn-start');
 const $contRelay = document.querySelector('.cont-relay');
 const $inpWord = document.querySelector('.word-input');
+const $turnOn = document.querySelector('.turn');
+const $turnOn2 = document.querySelector('.turn2');
+const $putWord = document.querySelector('.put-word');
+const $putWord2 = document.querySelector('.put-word2');
+const $inputWord = document.querySelector('.input-text');
+const $submit = document.querySelector('.submit');
 
 const randomWords = [
     '호랑이',
@@ -14,10 +20,42 @@ const randomWords = [
     '대학교',
 ];
 
+let words;
+
 $startBtn.addEventListener('click', function () {
     $contRelay.classList.remove('hide');
     $startBtn.classList.add('hide');
     $inpWord.textContent =
         '제시어 : ' +
         randomWords[Math.floor(Math.random() * randomWords.length)];
+});
+
+$submit.addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log(Number($putWord.textContent));
+    $inpWord.classList.add('hide');
+    if (Number($putWord.textContent) == 0) {
+        if ($inputWord.value == '') {
+            alert('값을 입력해주세요');
+        } else {
+            words = $inputWord.value;
+            $inputWord.value = null;
+            $putWord.textContent = words;
+            $turnOn2.textContent = '니 차례야!';
+            $turnOn.innerHTML = '&nbsp';
+            $putWord2.innerHTML = '&nbsp';
+        }
+    } else if (Number($putWord2.textContent) == 0) {
+        if ($inputWord.value == '') {
+            alert('값을 입력해주세요');
+        } else {
+            $putWord.innerHTML = '&nbsp;';
+            words = $inputWord.value;
+            $inputWord.value = null;
+            $putWord2.textContent = words;
+            $turnOn.textContent = '니 차례야!';
+            $turnOn2.innerHTML = '&nbsp';
+            $putWord.innerHTML = '&nbsp';
+        }
+    }
 });
